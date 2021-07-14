d0da3488dcdca0c17178c0f1404d43eafccaf71d
Leonardo Fragozo <lqfragozo@gmail.com>
2021-07-13 21:35:38 -0300

    more /index tests

--- spec/requests/api/v1/countries_spec.rb
+++ spec/requests/api/v1/countries_spec.rb
@@ -7,5 +7,13 @@ RSpec.describe "Api::V1::Countries", type: :request do
         get '/api/v1/countries'
         expect(response).to have_http_status(:ok)
       end
+       
+      it 'must return the current country data' do
+        country = create(:country)
+        get '/api/v1/countries'  
+        expect(json_body[:data][0]).to have_key(:id)
+        expect(json_body[:data][0]).to have_key(:name)
+      end
+      
     end
   end
