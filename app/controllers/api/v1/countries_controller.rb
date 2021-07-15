c65c0e6c0f85e6c53c0b6cb5f8489eeeb20d0e23
Leonardo Fragozo <lqfragozo@gmail.com>
2021-07-15 20:07:53 -0300

    endpoint show done

--- app/controllers/api/v1/countries_controller.rb
+++ app/controllers/api/v1/countries_controller.rb
@@ -5,9 +5,21 @@ module Api
   class CountriesController < ApplicationController
 
     def index
-    countries = Country.order('created_at ASC')
-    render json: { status: 'SUCCESS', message: 'Países carregados', data: countries },
+      countries = Country.order('created_at ASC')
+      render json: { status: 'SUCCESS', message: 'Países carregados', data: countries },
            status: :ok
+    end
+
+    def show
+      country = Country.find(params[:id])
+      render json: { status: 'SUCCESS', message: 'Países carregados', data: country },
+             status: :ok
+    end
+
+    private
+
+    def country_params
+      params.require(:country).permit(:id, :name)
     end
 
   end
