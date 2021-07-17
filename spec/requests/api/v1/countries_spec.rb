0889a4228c1f4515f8df6ac24cf2f38416a0a271
Leonardo Fragozo <lqfragozo@gmail.com>
2021-07-17 17:36:57 -0300

    show specs done

--- spec/requests/api/v1/countries_spec.rb
+++ spec/requests/api/v1/countries_spec.rb
@@ -22,6 +22,7 @@ RSpec.describe "Api::V1::Countries", type: :request do
         expect(json_body[:data][0][:name]).to eq('Brasil' )
         expect(json_body[:data][1][:name]).to eq('Japan' )
         expect(json_body[:data][2][:name]).to eq('EUA' )
+
       end
  
       describe 'GET #show' do
@@ -31,12 +32,11 @@ RSpec.describe "Api::V1::Countries", type: :request do
           expect(response).to have_http_status(:ok)
           end
 
-          it 'must return Brazil data' do
-            country = create(:country)
-            get "/api/v1/countries/1"
-            expect(json_body[:data][:name]).to eq('Brasil' )
+          it 'must return Japan data' do
+            country = create(:country, name: 'Japan')
+            get "/api/v1/countries/#{country.id}"
+            expect(json_body[:data][:name]).to eq('Japan' )
           end
-        
       end
+    end
   end
-end
